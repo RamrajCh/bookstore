c48a9cb364a587ca950ff4162da11e52f65a5c11
Ramraj.Chimouriya <ramraj.cedar@gmail.com>
2023-05-05 17:04:03 +0545

    Add project files.

---
+++ BookStore/Shared/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Shared
+{
+	public class Book
+	{
+		public int BookId { get; set; }
+		public string? Title { get; set; }
+		public string? Author { get; set; }
+		public string? ISBN { get; set; }
+		public DateTime PublishedYear { get; set; }
+		public decimal Price { get; set; }
+		public int Stock { get; set; }
+		public Genre? Genre { get; set; }
+		public int GenreId { get; set; }
+
+	}
+}
